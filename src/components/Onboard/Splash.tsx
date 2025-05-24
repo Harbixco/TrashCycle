b15730eb12e72d123f8dac0598f2c7e3ad60ca7e
hagios <agbajeabiodunkay@gmail.com>
2025-05-24 17:27:01 +0100

    splash and other pages well arranged

--- src/components/Onboard/Splash.tsx
+++ src/components/Onboard/Splash.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { slide1 } from "../../assets";
+import { Link } from "react-router-dom";
 
 export default function Splash() {
   return (
-    <div className='text-2xl'>Welocme To Splash</div>
-  )
+    <div className=" min-h-screen w-full  bg-[#27662A]">
+      <div className="flex justify-center pt-40">
+        <img src={slide1} alt="slide1" />
+      </div>
+
+      <div className="my-5 flex justify-center">
+        <div className="text-white">
+          <div className="pl-5">
+            Schedule trash pickup with just a few clicks.
+          </div>
+
+          <div className="pr-10">
+            {" "}
+            We collect your waste quickly, cleanly, and on time.
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center pt-10">
+        <button
+          type="button"
+          className="rounded-lg bg-[#79AD27] px-5 py-2 text-sm font-semibold text-white hover:bg-green-800 hover:text-white"
+        >
+          <Link to="/educate">Get Started</Link>
+        </button>
+      </div>
+    </div>
+  );
 }
