d549a6fd37429088fb6164d36040eb4e21e36cda
hagios <agbajeabiodunkay@gmail.com>
2025-06-04 05:27:11 +0100

    worked on payment, pickup and other components

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Dashboard } from "./Dashboard"
 export {default as DashboardLayout} from "./DashboardLayout"
+export {default as DashboardMobileSidebar} from "./DashboardMobileSidebar"
